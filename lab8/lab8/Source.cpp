ce362f229f0b2b29b69121381946350b5ef22601
Dima Gaykov <dima.gaykov.04@gmail.com>
2022-03-20 01:35:03 +0300

    8 lab with dops

--- lab8/lab8/Source.cpp
+++ lab8/lab8/Source.cpp
@@ -1,7 +1,16 @@
-//Создать очередь для символов и функции для ввода, вывода и определения размера очереди.
+//1) Создать очередь для символов и функции для ввода, вывода и определения размера очереди.
 //Ввести символы с экрана в очередь.В случае совпадения вводимого символа с последним
 //элементом оче - реди удалить первых три элемента.
+// 
 // Максимальный размер очереди ввести с клавиатуры.
+// 
+//2) Создать очередь для символов и функции для ввода, вывода и удаления элементов очереди.
+//Ввести символы с клавиатуры в очередь.После введения 5го символа в ответ на каждый
+//вводимый удалять по одному элементу из очереди.
+// 
+//3) Создать очередь для символов и функции для ввода, вывода, удаления и определения размера
+//очереди. Ввести символы с экрана в очередь. В случае совпадения вводимого символа с первым
+//элементом очереди вывести оче-редь и ее размер. 
 #include <iostream>
 #include <windows.h>
 #include "check.h"
@@ -24,12 +33,12 @@ void input(char s)
 	symbs->end = newSymb;
 	symbs = start;
 }
-void output()
+void output(int  i = 1)
 {
 	symbs = symbs->next;
-	cout << symbs->symb << ";\t";
+	cout << i++ << ") " << symbs->symb << ";\t";
 	if (symbs->next != symbs->end)
-		return output();
+		return output(i);
 	symbs = start;
 }
 bool research(char s)
@@ -67,6 +76,32 @@ void del()
 	}
 	symbs = start;
 }
+void del2()
+{
+	Char* temp;
+	cnt--;
+	symbs = symbs->next;
+	temp = symbs->next;
+	delete symbs;
+	symbs = temp;
+	start->next = symbs;
+	symbs = start;
+}
+void del(int num)
+{
+	cnt--;
+	Char* preEl = start;
+	for (int i = 0; i < num; i++)
+	{
+		preEl = symbs;
+		symbs = symbs->next;
+	}
+	symbs != symbs->end ? preEl->next = symbs->next : symbs->end = preEl;
+	Char* temp = symbs->next;
+	delete symbs;
+	symbs = temp;
+	symbs = start;
+}
 
 int main()
 {
@@ -99,18 +134,20 @@ int main()
 			<< "2. Output queue\n"
 			<< "3. Show queue size\n"
 			<< "4. Enter element to delete 3 first elements!\n"
-			<< "5. Exit!\n"
+			<< "5. Delete elements!\n"
+			<< "6. Exit!\n"
 			<< "Enter menu point needed you: ";
 		getline(cin, mN);
 		
-		while (!isProved(mN, 1, 5))
+		while (!isProved(mN, 1, 6))
 		{
 			cls;
 			cout << "1. Input symbol to queue\n"
 				<< "2. Output queue\n"
 				<< "3. Show queue size\n"
 				<< "4. Enter element to delete 3 first elements!\n"
-				<< "5. Exit!\n"
+				<< "5. Delete elements!\n"
+				<< "6. Exit!\n"
 				<< "Enter menu point needed you: ";
 			getline(cin, mN);
 		}
@@ -121,7 +158,9 @@ int main()
 		{
 		case 1: // input
 		{
-
+			if(cnt == 5)
+				del2();
+			
 			cnt++;
 			if (cnt > MAX)
 			{
@@ -142,6 +181,15 @@ int main()
 
 			symb = mStr[0];
 			input(symb);
+
+			if (cnt > 1 && start->next->symb == mStr[0])
+			{
+				cout << "It was entered the symbol, that is first in queue!\n"
+					<< "Queue size = " << cnt << endl;
+				output();
+				ent;
+				pause;
+			}
 			break;
 		}
 		case 2: // output
@@ -186,7 +234,32 @@ int main()
 			pause;
 			break;
 		}
-		case 5: // Exit
+		case 5: // delete
+		{
+			if (cnt == 0)
+			{
+				cout << "Fill queue!" << endl;
+				pause;
+				break;
+			}
+			output();
+			ent;
+			ent;
+			cout << "Choise the element point to delete its: ";
+			getline(cin, mN);
+
+			while (!isProved(mN, 1, cnt))
+			{
+				cls;
+				cout << "Choise the element point to delete its(from 1 to " << cnt << "): ";
+				getline(cin, mN);
+			}
+
+			del(stoi(mN));
+
+			break;
+		}
+		case 6: // Exit
 		{
 			isExit = true;
 			pause;
